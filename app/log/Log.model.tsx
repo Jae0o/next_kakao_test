b0da4fd2f0f03e6e43ad20110206ab5ae66ea7d8
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-12 16:28:22 +0900

    Store 변경 중 3

--- app/log/Log.model.tsx
+++ app/log/Log.model.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Position } from "./Log.types";
-import { useSearchParams } from "next/navigation";
 
 const DEFAULT_CENTER_POSITION = {
   lat: 33.450701,
@@ -8,9 +7,7 @@ const DEFAULT_CENTER_POSITION = {
 };
 
 const useLogModel = () => {
-  const searchParams = useSearchParams();
   const [center, setCenter] = useState<Position>(DEFAULT_CENTER_POSITION);
-
   const [path, setPath] = useState<Position[]>([]);
   const [isRecording, setIsRecording] = useState(false);
   const [pathFetchCount, setPathFetchCount] = useState(0);
--- store/useCurrentLocatuon.tsx
+++ store/useCurrentLocatuon.tsx
@@ -1,12 +1,19 @@
-import { create, useStore } from "zustand";
+import { create } from "zustand";
 import { Position } from "../app/log/Log.types";
+
+const DEFAULT_CENTER_POSITION = {
+  lat: 33.450701,
+  lng: 126.570667,
+};
 
 interface UseUserLocation {
   currentLocation: Position;
   watchCode: number;
 
+  watchSuccess: (param: GeolocationPosition) => void;
   fetchLocation: (param: Position) => void;
   setWatchCode: (code: number) => void;
+  deleteWatch: (code: number) => void;
 }
 
 export const useUserLocation = create<UseUserLocation>((set) => ({
@@ -16,11 +23,17 @@ export const useUserLocation = create<UseUserLocation>((set) => ({
   },
   watchCode: 0,
 
+  watchSuccess: () => {},
+
   fetchLocation: ({ lat, lng }) => {
     set(() => ({ currentLocation: { lat, lng } }));
   },
 
   setWatchCode: (newCode) => {
     set(() => ({ watchCode: newCode }));
+  },
+
+  deleteWatch: (code) => {
+    navigator.geolocation.clearWatch(code);
   },
 }));
