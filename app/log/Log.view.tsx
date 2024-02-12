cdf28d31051b405ffe1299dc1e12bb15b05a4429
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-12 13:02:17 +0900

    실제 환경과 유사하게 스타일 변경

--- app/log/Log.view.tsx
+++ app/log/Log.view.tsx
@@ -26,18 +26,6 @@ const LogView = ({
 }: LogViewProps) => {
   return (
     <section className={style.log__layout}>
-      {isRecording && (
-        <button onClick={endRecord} className={style.log_button}>
-          end
-        </button>
-      )}
-
-      {!isRecording && (
-        <button onClick={startRecord} className={style.log_button}>
-          start
-        </button>
-      )}
-
       <Map center={center} className={style.log__map}>
         <MapMarker position={center} />
         {path.length !== 0 && (
@@ -50,12 +38,41 @@ const LogView = ({
           />
         )}
       </Map>
-      <h1 className={style.log__count}>
-        Center Fetch Count : {centerFetchCount}
-      </h1>
-      <h1 className={style.log__count}>Path Fetch Count : {pathFetchCount}</h1>
-      <h1 className={style.log__count}>Error Count : {errorCount}</h1>
-      <h1 className={style.log__count}>Path Length Count : {path.length}</h1>
+
+      {/* Count Information Container */}
+      <ul className={style.log__count_list}>
+        <li className={style.log__count_item}>
+          Center Fetch Count : {centerFetchCount}
+        </li>
+        <li className={style.log__count_item}>
+          Path Fetch Count : {pathFetchCount}
+        </li>
+        <li className={style.log__count_item}>
+          Path Array Length Count : {path.length}
+        </li>
+        <li className={style.log__count_item}>Error Count : {errorCount}</li>
+      </ul>
+
+      <div className={style.log__action_container}>
+        {/* Start Button */}
+        {!isRecording && (
+          <button onClick={startRecord} className={style.log_button}>
+            start
+          </button>
+        )}
+
+        {isRecording && (
+          <>
+            <div className={style.log__record_container}>
+              <p className={style.log__record}> 25 : 25 : 25</p>
+              <p className={style.log__record}> 100.40 km</p>
+            </div>
+            <button onClick={endRecord} className={style.log_button}>
+              end
+            </button>
+          </>
+        )}
+      </div>
     </section>
   );
 };
